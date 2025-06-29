bb6d6ac88c3003a2f9a6f5f53ed14d600cffb5da
Xylar Asay-Davis <xylarstorm@gmail.com>
2025-06-29 10:59:19 -0500

    Add preprocessing step for topography
    After downloading the data, we need to:
    * rename variables to have consistent names
    * compute the ice draft
    * compute ice, ocean, grounded, floating and rock fractions
    These are all computed on the original grid before remapping.
    
    This merge also includes a new check method that ensures that
    the required fields are available in the preprocessed dataset.

--- i7aof/topo/bedmachine.py
+++ i7aof/topo/bedmachine.py
@@ -1,5 +1,8 @@
 import os
 
+import numpy as np
+import xarray as xr
+
 from i7aof.remap import remap_projection_to_ismip
 from i7aof.topo.topo_base import TopoBase
 
@@ -16,23 +19,29 @@ class BedMachineAntarcticaV3(TopoBase):
     about Bedmachine Antarctica v3 topography data.
     """
 
-    def download_topo(self):
+    def download_and_preprocess_topo(self):
         """
         Download the original topography file.
         """
-        self.get_orig_topo_path()
-
-    def get_orig_topo_path(self):
-        """
-        Get the path to the original topography file before remapping
-        """
-        filename = os.path.join('topo', data_filename)
-        if not os.path.exists(filename):
+        download_filename = os.path.join('topo', data_filename)
+        if not os.path.exists(download_filename):
             raise FileNotFoundError(
-                f'File {filename} not found. Please download manually from '
-                f'{data_url}, as we are not authorized to download it for '
-                f'you.'
+                f'File {download_filename} not found. Please download '
+                f'manually from {data_url}, as we are not authorized to '
+                f'download it for you.'
             )
+
+        out_filename = self.get_preprocessed_topo_path()
+        self._preprocess_topo(download_filename, out_filename)
+        super().download_and_preprocess_topo()
+
+    def get_preprocessed_topo_path(self):
+        """
+        Get the path to the preprocessed topography file before remapping
+        """
+        filename = os.path.join(
+            'topo', data_filename.replace('.nc', '_processed.nc')
+        )
         return filename
 
     def get_topo_on_ismip_path(self):
@@ -50,7 +59,7 @@ class BedMachineAntarcticaV3(TopoBase):
         Remap the topography to the ISMIP grid."
         """
         remap_projection_to_ismip(
-            in_filename=self.get_orig_topo_path(),
+            in_filename=self.get_preprocessed_topo_path(),
             in_mesh_name='bedmachine_antarctica_v3',
             in_proj4='epsg:3031',
             out_filename=self.get_topo_on_ismip_path(),
@@ -59,3 +68,64 @@ class BedMachineAntarcticaV3(TopoBase):
             config=self.config,
             logger=self.logger,
         )
+
+    def _preprocess_topo(self, in_filename, out_filename):
+        """
+        Preprocess the topography file before remapping.
+
+        Parameters
+        ----------
+        in_filename : str
+            The input filename to preprocess.
+        out_filename : str
+            The output filename after preprocessing.
+        """
+        ds_in = xr.open_dataset(in_filename)
+        vars_to_copy = ['bed', 'surface', 'thickness']
+
+        ds_out = ds_in[vars_to_copy]
+        ds_out.attrs = ds_in.attrs
+
+        ds_out['draft'] = ds_out['surface'] - ds_out['thickness']
+        ds_out.draft.attrs = {
+            'long_name': 'Ice draft',
+            'units': 'meters',
+        }
+
+        # mask:
+        # 0: open ocean
+        # 1: ice_free_land
+        # 2: grounded_ice
+        # 3: floating_ice
+        # 4: lake_vostok
+        mask = ds_in.mask
+
+        ds_out['ice_frac'] = np.logical_and(mask != 0, mask != 1).astype(float)
+        ds_out.ice_frac.attrs = {
+            'long_name': 'Area Fraction of Ice',
+            'units': '1',
+        }
+        ds_out['ocean_frac'] = np.logical_or(mask == 0, mask == 3).astype(
+            float
+        )
+        ds_out.ocean_frac.attrs = {
+            'long_name': 'Area Fraction of Ocean',
+            'units': '1',
+        }
+        ds_out['grounded_frac'] = (mask == 2).astype(float)
+        ds_out.grounded_frac.attrs = {
+            'long_name': 'Area Fraction of Grounded Ice',
+            'units': '1',
+        }
+        ds_out['floating_frac'] = (mask == 3).astype(float)
+        ds_out.floating_frac.attrs = {
+            'long_name': 'Area Fraction of Floating Ice',
+            'units': '1',
+        }
+        ds_out['rock_frac'] = (mask == 1).astype(float)
+        ds_out.rock_frac.attrs = {
+            'long_name': 'Area Fraction of Bare Rock',
+            'units': '1',
+        }
+
+        ds_out.to_netcdf(out_filename)
